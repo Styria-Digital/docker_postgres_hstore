070065db477b548c1e417e136fb3a0d49f88ae76
Matija Petrunić <matija.petrunic@gmail.com>
2019-01-15 15:33:45 +0100

    Add supported postgres versions

---
+++ 10/add_hstore.sh
@@ -0,0 +1,9 @@
+# Add hstore to already created database
+"${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS hstore;
+EOSQL
+
+# Add hstore to template so all created databases will contain hstore
+"${psql[@]}" --dbname="template1" <<-'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS hstore;
+EOSQL
