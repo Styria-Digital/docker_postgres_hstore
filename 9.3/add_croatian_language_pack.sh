1ca86385be70a145140cc667904c4b79e2f8f8d6
Filip Todic <filip.todic@styria.hr>
2019-01-17 16:07:09 +0100

    Add the Croatian language dictionary for Full Text Searc

---
+++ 9.3/add_croatian_language_pack.sh
@@ -0,0 +1,47 @@
+# Add hstore to already created database
+"${psql[@]}" --dbname="$POSTGRES_DB" <<-'EOSQL'
+    -- create croatian ispell search dictionary
+    CREATE TEXT SEARCH DICTIONARY croatian_ispell (
+        TEMPLATE = ispell,
+        DictFile = croatian,
+        AffFile = croatian,
+        StopWords = croatian
+    );
+
+    -- create croatian search configuration based on 'simple' configuration
+    CREATE TEXT SEARCH CONFIGURATION croatian ( COPY = pg_catalog.simple );
+
+    -- Change croatian search configuration to use our croatian_ispell dictionary we created before
+    ALTER TEXT SEARCH CONFIGURATION croatian
+        ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
+                        word, hword, hword_part
+        WITH croatian_ispell;
+
+    -- Remove fulltext search mappings for special types
+    ALTER TEXT SEARCH CONFIGURATION croatian
+        DROP MAPPING FOR email, url, url_path, sfloat, float;
+EOSQL
+
+# Add hstore to template so all created databases will contain hstore
+"${psql[@]}" --dbname="template1" <<-'EOSQL'
+    -- create croatian ispell search dictionary
+    CREATE TEXT SEARCH DICTIONARY croatian_ispell (
+        TEMPLATE = ispell,
+        DictFile = croatian,
+        AffFile = croatian,
+        StopWords = croatian
+    );
+
+    -- create croatian search configuration based on 'simple' configuration
+    CREATE TEXT SEARCH CONFIGURATION croatian ( COPY = pg_catalog.simple );
+
+    -- Change croatian search configuration to use our croatian_ispell dictionary we created before
+    ALTER TEXT SEARCH CONFIGURATION croatian
+        ALTER MAPPING FOR asciiword, asciihword, hword_asciipart,
+                        word, hword, hword_part
+        WITH croatian_ispell;
+
+    -- Remove fulltext search mappings for special types
+    ALTER TEXT SEARCH CONFIGURATION croatian
+        DROP MAPPING FOR email, url, url_path, sfloat, float;
+EOSQL
